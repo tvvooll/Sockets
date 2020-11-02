69c725dc29d1aeaf44b3ba1865d6e2f50299e82e
Tamara Volcharenko <vtvo16@gmail.com>
2020-11-02 19:07:15 +0200

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        //Сокет для взаимодействия с сервером.
+        private static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        //Массив для хранения отправляемого сообщения.
+        private static byte[] buffer = new byte[1024]; 
+        static void Main(string[] args)
+        {
+            //Подключаемся к точке сети к которой привязан наш сервер и оповещаем пользователя в случае удачи.
+            socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000));
+            Console.WriteLine("Connected to server.");
+
+            //Даём пользователю возможность неограниченого долго слать сообщения на сервер.
+            //Выхода не будет - это маркетинг. В следующий раз нельзя будет даже на крестик нажать :)
+            while (true)
+            {
+                //Считываемм сообщение от пользователя, кодируем их в UTF-8, сохраняя в заранее подготовленый буффер.
+                Console.Write("Type your message: ");
+                string message = Console.ReadLine();
+                buffer = Encoding.UTF8.GetBytes(message);
+
+                //Отправляем закодированное сообщение на сервер.
+                socket.Send(buffer);
+
+                //Обнуляем буффер дабы не отправить муссор при следующем сообщении к серверу.
+                buffer = new byte[1024];
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        //Сокет для взаимодействия с клиентом.
+        private static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        //Массив для передачи в метод получения данных от клиента.
+        private static byte[] buffer = new byte[1024];
+        static void Main(string[] args)
+        {
+            //Привязываем сокет к конечной точке сети, с адресом 127.0.0.1 (соответствует локалхосту) и портом 2000.
+            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2000));
+
+            //Переводим сокет в режим прослушивания (ожидания подключений) и оповещаем пользователя об этом.
+            socket.Listen(5);
+            Console.WriteLine("Server started.");
+
+            //Ожидаем клиента и принимаем его.
+            Socket client = socket.Accept();
+            Console.WriteLine("New user connected.");
+
+            //Ввиду слова "простейший" в условии лабы делаем наш сервер однопользовательским
+            while(true)
+            {
+                //Принимаем данные от клиента помещая их в заранее заготовленный буффер.
+                int n = client.Receive(buffer);
+
+                //Декодируем сообщение из байтов в строку.
+                string message = Encoding.UTF8.GetString(buffer, 0, n);
+
+                //Обнуляем буффер дабы не поймать муссор при следующих приёмах данных от клиента.
+                buffer = new byte[1024];
+
+                //Выводим сообщение пользователя.
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
